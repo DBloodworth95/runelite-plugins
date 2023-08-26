2c8c22ca07a1e1c7e24a630ac07a02f78c8bf856
DBloodworth95 <dan.bloodworth95@gmail.com>
2023-08-26 19:12:12 +0100

    Add ClientHijackDownloader

---
+++ src/main/java/net/runelite/client/plugins/bootstrap/ClientHijackDownloader.java
@@ -0,0 +1,17 @@
+package net.runelite.client.plugins.bootstrap;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class ClientHijackDownloader {
+    public static void download(String sourceUrl, String destinationPath) throws IOException {
+        ReadableByteChannel readableByteChannel = Channels.newChannel(new URL(sourceUrl).openStream());
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(destinationPath)) {
+            fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+        }
+    }
+}
